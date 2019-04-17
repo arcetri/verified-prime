d6b26cfdaed88632d5ea16dbb1bb6e64fcb2b5b8
Landon Curt Noll <github-mail@asthe.com>
2019-04-17 03:05:44 -0500

    Uploaded job data sets

---
+++ v1-data/job.h-not0mod3.prime-large/run.all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# run.all.sh - run all slurm jobs for job.h-not0mod3.prime-large
+#
+cd /usr/global/src/cisco/verified-prime/v1-data/job.h-not0mod3.prime-large
+sbatch sbatch.aaaaa.slurm
+sbatch sbatch.aaaab.slurm
+sbatch sbatch.aaaac.slurm
+sbatch sbatch.aaaad.slurm
+sbatch sbatch.aaaae.slurm
+sbatch sbatch.aaaaf.slurm
+sbatch sbatch.aaaag.slurm
+sbatch sbatch.aaaah.slurm
+sbatch sbatch.aaaai.slurm
+sbatch sbatch.aaaaj.slurm
+sbatch sbatch.aaaak.slurm
+sbatch sbatch.aaaal.slurm
+sbatch sbatch.aaaam.slurm
+sbatch sbatch.aaaan.slurm
+sbatch sbatch.aaaao.slurm
+sbatch sbatch.aaaap.slurm
+sbatch sbatch.aaaaq.slurm
+sbatch sbatch.aaaar.slurm
+sbatch sbatch.aaaas.slurm
+sbatch sbatch.aaaat.slurm
+sbatch sbatch.aaaau.slurm
+sbatch sbatch.aaaav.slurm
+sbatch sbatch.aaaaw.slurm
+sbatch sbatch.aaaax.slurm
+sbatch sbatch.aaaay.slurm
+sbatch sbatch.aaaaz.slurm
+sbatch sbatch.aaaba.slurm
+sbatch sbatch.aaabb.slurm
+sbatch sbatch.aaabc.slurm
+sbatch sbatch.aaabd.slurm
+sbatch sbatch.aaabe.slurm
+sbatch sbatch.aaabf.slurm
+sbatch sbatch.aaabg.slurm
+sbatch sbatch.aaabh.slurm
